cf62d045ec98ac62bf2634b13378655c3d59fb99
Dickens-Odera <dickensodera9@gmail.com>
2022-01-21 19:45:13 +0300

    feat: get latest ETH/USD price

--- contracts/PriceFeedConsumer.sol
+++ contracts/PriceFeedConsumer.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 
 import './interfaces/IPriceFeedConsumer.sol';
 
@@ -16,10 +17,18 @@ contract PriceFeedConsumer is Ownable, IPriceFeedConsumer{
     address internal immutable OSQTH_ADDRESS = 0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B;
     address internal immutable ETH_USD_ORACLE = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
 
-    constructor() public{}
+    AggregatorV3Interface internal priceFeed; //instantiate an oracle pricefeed
 
-    function getEthPrice() public view override returns(uint){
+    constructor() public{
+        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    }
 
+    /**
+    * @dev get the latest price of ETH in USD from the oracle price feed
+    */
+    function getEthPrice() public view override returns(int){
+        (uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound) = priceFeed.latestRoundData();
+        return price;
     }
 
     function getEth2Price() public view override returns(uint){
--- contracts/interfaces/IPriceFeedConsumer.sol
+++ contracts/interfaces/IPriceFeedConsumer.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IPriceFeedConsumer{
-    function getEthPrice() external view returns(uint);
+    function getEthPrice() external view returns(int);
     function getEth2Price() external view returns(uint);
     function getMarkPrice() external view returns(uint);
     function getOSQthPrice() external view returns(uint);
