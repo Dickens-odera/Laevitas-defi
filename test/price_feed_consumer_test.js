0b9aaeb87547daad9146837d9da9b7c405e83ab5
Dickens-Odera <dickensodera9@gmail.com>
2022-01-22 20:55:01 +0300

    feat: mock test setup

--- test/price_feed_consumer_test.js
+++ test/price_feed_consumer_test.js
@@ -1,18 +1,40 @@
 const PriceFeedConsumer = artifacts.require("PriceFeedConsumer");
+const MockController = artifacts.require('MockSqueethController');
+const MockOracle = artifacts.require('MockSqueethOracle');
+const MockUSDC = artifacts.require('MockUSDC');
+const MockWETH = artifacts.require('MockWETH');
+const MockosQueethToken = artifacts.require('MockoSQUEETH');
+
 
 contract("PriceFeedConsumer",  (accounts) => {
   let priceFeedConsumer;
-  let oraclePriceFeed;
+  let chainLinkOraclePriceFeed;
+  let mockController;
+  let mockOracle;
+  let mockUSDC;
+  let mockWETH;
+  let mockoSQTH;
 
   beforeEach(async() => {
     [owner, alice, bob ] = accounts;
     priceFeedConsumer = await PriceFeedConsumer.deployed();
-    oraclePriceFeed = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419';
+    chainLinkOraclePriceFeed = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419';
+    mockController = await MockController.deployed();
+    mockOracle = await MockOracle.deployed();
+    mockUSDC = await MockUSDC.deployed();
+    mockWETH = await MockWETH.deployed();
+    mockoSQTH = await MockosQueethToken.deployed();
   });
 
   describe("deployment", () => {
     it("deploys successfully", async() => {
       assert(priceFeedConsumer,"Price Feed Consumer contract deployed successfully");
+    });
+  });
+
+  describe("mocks", async() => {
+    it('can set the index price(ETH^2)', async() => {
+
     });
   });
 
