0c1693de7311be9102585a0e95587a682416c26b
Dickens-Odera <dickensodera9@gmail.com>
2022-01-22 18:52:35 +0300

    update: remove exiting Ownable contract dependency

--- contracts/mocks/MockWETH.sol
+++ contracts/mocks/MockWETH.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.0;
 pragma abicoder v2;
 
-import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract MockWETH is Ownable, ERC20{
+contract MockWETH is ERC20{
     constructor(uint _initialSupply) ERC20("Mock WETH","mWETH"){
         _mint(msg.sender,_initialSupply * 10 ** decimals());
     }
