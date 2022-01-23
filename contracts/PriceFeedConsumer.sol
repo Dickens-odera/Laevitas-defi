4a4a81a5a7490429ca5b60c5532feb805a602355
Dickens-Odera <dickensodera9@gmail.com>
2022-01-24 00:00:42 +0300

    update: final deployment to the ropsten network

--- contracts/PriceFeedConsumer.sol
+++ contracts/PriceFeedConsumer.sol
@@ -47,13 +47,13 @@ contract PriceFeedConsumer is IPriceFeedConsumer,Ownable{
     using SafeMath for uint256;
     
     address internal constant OSQTH_TOKEN_ADDRESS = 0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B;
-    address internal constant CHAINLINK_KOVAN_ETH_USD_ORACLE = 0x9326BFA02ADD2366b30bacB125260Af641031331;
     address internal constant SQUEETH_CONTROLLER = 0x64187ae08781B09368e6253F9E94951243A493D5;
     address internal constant SQUEETH_ORACLE = 0x65D66c76447ccB45dAf1e8044e918fA786A483A1;
     address internal constant SQU_ETH_UNI_V3_POOL = 0x82c427AdFDf2d245Ec51D8046b41c4ee87F0d29C;
     address internal constant ETH_USDC_UNI_V3_POOL = 0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8;
     address internal constant WETH_TOKEN_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address internal constant USDC_TOKEN_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public immutable CHAINLINK_KOVAN_ETH_USD_ORACLE = 0x9326BFA02ADD2366b30bacB125260Af641031331;
 
     AggregatorV3Interface internal priceFeed; //instantiate an oracle pricefeed
 
